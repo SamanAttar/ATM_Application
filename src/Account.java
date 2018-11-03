66a84df469d27867853c5666b3efbdb8452b1717
Saman Attar <saman.attar@outlook.com>
2018-11-03 15:30:13 -0500

    Add constructor for Account

--- src/Account.java
+++ src/Account.java
@@ -3,10 +3,26 @@ import java.util.ArrayList;
 public class Account {
 
     private String name;
-    private double balance;
+
     private String uuid;
 
     private User owner;
 
     private ArrayList<Transaction> transactions;
+
+
+    public Account(String name, User user, Bank bank){
+        this.name = name;
+        this.owner = user;
+        this.uuid = bank.getNewAccountId();
+
+        this.transactions = new ArrayList<Transaction>();
+
+        user.addAccount(this);
+        bank.addAccount(this);
+    }
+
+    public String getUUID(){
+        return this.uuid;
+    }
 }
